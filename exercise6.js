78c4e325b84a979f9f6e0db7193dcfe3d7040124
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-05-29 08:13:52 +0700

    Update exercise6.js

--- exercise6.js
+++ exercise6.js
@@ -12,3 +12,16 @@ while(b >=2){
   console.log(b+' - I Will Become Full Stack Developer');
   b-=2;
 }
+
+// No 2
+
+console.log('LOOPING PERTAMA');
+for(var a =1; a<=20; a++){
+  console.log(a+ ' - I Love Coding');
+}
+
+console.log('LOOPING KEDUA')
+for(var b =20; b>=1; b--){
+  console.log(b+ ' - I Will Become Full Stack Developer');
+}
+
