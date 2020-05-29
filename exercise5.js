ef2d8281cd333a1ba2fe50e54f449083d55c2553
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-05-29 19:56:35 +0700

    Update exercise5.js

--- exercise5.js
+++ exercise5.js
@@ -24,3 +24,18 @@ console.log('Second Word: ' + secondWord);
 console.log('Third Word: ' + thirdWord);
 console.log('Fourth Word: ' + fourthWord);
 console.log('Fifth Word: ' + fifthWord);
+
+// Bagian 3
+
+var word = 'wow JavaScript is so cool';
+var firstWord = word.substring(0,3);
+var secondWord = word.substring(4,14);
+var thirdWord = word.substring(15,17);
+var fourthWord = word.substring(18,20);
+var fifthWord = word.substring(21,25);
+
+console.log('First Word: ' + firstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
