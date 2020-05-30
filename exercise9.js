f1243c3f52b405953896b400854364136a4cc80a
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-05-30 15:21:04 +0700

    Update exercise9.js

--- exercise9.js
+++ exercise9.js
@@ -4,3 +4,14 @@ function shoutOut(){
   return 'Halo Function!';
 }
 console.log(shoutOut());
+
+//Tugas 2
+
+function calculateMultiply(num1,num2){
+  return num1*num2;
+}
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
