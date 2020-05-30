bc5a62fb96b33169edfe18c752b2a7f2f3d84924
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-05-30 08:57:38 +0700

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,1 +1,50 @@
 
+var tanggal = 8
+var bulan = 13
+var tahun = 2000
+if(bulan <13){
+  switch (bulan){
+  case 1:
+  bulan = 'Januari';
+  break;
+  case 2:
+  bulan ='Febuari';
+  break;
+  case 3:
+  bulan ='Maret';
+  break;
+  case 4:
+  bulan ='April';
+  break;
+  case 5:
+  bulan ='Mei';
+  break;
+  case 6:
+  bulan ='Juni';
+  break;
+  case 7:
+  bulan ='Juli';
+  break;
+  case 8:
+  bulan ='Agustus';
+  break;
+  case 9:
+  bulan ='September';
+  break;
+  case 10:
+  bulan ='Oktober';
+  break;
+  case 11:
+  bulan ='November';
+  break;
+  case 12:
+  bulan ='Desember';
+  break;}
+}else{
+  bulan =', bulannya salah!'
+}
+if ( tanggal <32 && tahun <=2200){
+  console.log( 'Tanggalnya: '+tanggal +' '+ bulan + ', Tahunnya: '+ tahun)
+}else{
+  console.log('Tanggal Salah')
+}
