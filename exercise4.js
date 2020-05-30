1824b619e31a2fb7f085e48057dd03867c3b53db
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-05-30 09:18:50 +0700

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,49 +1,50 @@
-var tanggal = 8
-var bulan = 13
-var tahun = 1995
-if(bulan <13){
-  switch (bulan){
-  case 1:
-  bulan = 'Januari';
-  break;
-  case 2:
-  bulan ='Febuari';
-  break;
-  case 3:
-  bulan ='Maret';
-  break;
-  case 4:
-  bulan ='April';
-  break;
-  case 5:
-  bulan ='Mei';
-  break;
-  case 6:
-  bulan ='Juni';
-  break;
-  case 7:
-  bulan ='Juli';
-  break;
-  case 8:
-  bulan ='Agustus';
-  break;
-  case 9:
-  bulan ='September';
-  break;
-  case 10:
-  bulan ='Oktober';
-  break;
-  case 11:
-  bulan ='November';
-  break;
-  case 12:
-  bulan ='Desember';
-  break;}
+var tanggal = 8;
+var bulan = 13;
+var tahun = 1995;
+if (bulan <13){
+  switch(bulan){
+    case 1:
+      bulan ='januari';
+      break;
+      case 2:
+      bulan ='februari';
+      break;
+      case 3:
+      bulan ='maret';
+      break;
+      case 4:
+      bulan ='arpil';
+      break;
+      case 5:
+      bulan ='mei';
+      break;
+      case 6:
+      bulan ='juni';
+      break;
+      case 7:
+      bulan ='juli';
+      break;
+      case 8:
+      bulan ='agustus';
+      break;
+      case 9:
+      bulan ='september';
+      break;
+      case 10:
+      bulan ='oktober';
+      break;
+      case 11:
+      bulan ='november';
+      break;
+      case 12:
+      bulan ='desember';
+      break;      
+  }
 }else{
-  bulan =', bulannya salah!'
+  bulan = ', Bulanya salah!'
 }
-if ( tanggal <32 && tahun <=2200){
-  console.log( 'Tanggalnya: '+tanggal +' '+ bulan + ' Tahunnya: '+ tahun)
+if(tanggal <32 && tahun <=2200){
+  console.log('Tanggalnya: '+ tanggal + bulan + ', Tahunya: '+ tahun)
 }else{
   console.log('Tanggal Salah')
 }
