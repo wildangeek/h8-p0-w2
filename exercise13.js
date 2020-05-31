2419afff49510ce2e624d88b0b28f9e7155c4327
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-05-31 07:08:46 +0700

    Update exercise13.js

--- exercise13.js
+++ exercise13.js
@@ -1,1 +1,25 @@
-
+function xo (str){
+  var x =0;
+  var o = 0;
+  for (var i=0; i<=str.length; i++){
+    
+    if(str[i] === 'x'){
+      x+=1
+    }else if (str[i]){
+      o+=1
+    }else{
+      ''
+    }
+  }
+if(x===o){
+  return true
+}else{
+  return false
+}
+}
+  
+console.log(xo('xoxoxo')); 
+console.log(xo('oxooxo')); 
+console.log(xo('oxo'));
+console.log(xo('xxxooo')); 
+console.log(xo('xoxooxxo'));
